4aa39851823b0df431f88bedb84a81ed85d2f10a
Farhan Ahmed <farhan.ahmed@kitware.com>
2021-07-16 15:14:30 -0400

    Created output file writer script

--- scripts/redact.py
+++ scripts/redact.py
@@ -1,10 +1,11 @@
 import json
-import shutil
 import time
 from typing import List, BinaryIO
+from contextlib import contextmanager
+
 import pyvips
 import tifftools
-from contextlib import contextmanager
+from tqdm import tqdm
 
 
 class Polygon:
@@ -16,8 +17,8 @@ class Polygon:
 
 
 @contextmanager
-def vips_progress(vips_image):
-    def progress_print(image, progress):
+def vips_progress(vips_image: pyvips.Image):
+    def progress_print(image: pyvips.Image, progress):
         run = time.strftime('%M:%S', time.gmtime(progress.run))
         eta = time.strftime('%M:%S', time.gmtime(progress.eta))
         percent = progress.percent
@@ -38,35 +39,49 @@ def get_tiff_tag(info, tag):
     return info['ifds'][0]['tags'][tag]['data']
 
 
-def write_svs(dest: BinaryIO, src: BinaryIO, svg: pyvips.Image, offsets: List[int], lengths: List[int], srclen: int):
+def write_redacted_svs(
+    dest: BinaryIO,
+    original: BinaryIO,
+    original_offsets: List[int],
+    original_lengths: List[int],
+    original_srclen: int,
+    redacted: BinaryIO,
+    redacted_offsets: List[int],
+    redacted_lengths: List[int],
+    redacted_srclen: int,
+    is_redacted: List[bool],
+):
     COPY_CHUNKSIZE = 1024 ** 2
 
-    if len(offsets) != len(lengths):
+    if len(original_offsets) != len(original_lengths) or len(redacted_offsets) != len(redacted_lengths):
         raise Exception('Offsets and byte counts do not correspond')
     
-    destOffsets = [0] * len(offsets)
-    # We preserve the order of the chunks from the original file
-    offsetList = sorted([(offset, idx) for idx, offset in enumerate(offsets)])
-    olidx = 0
-    while olidx < len(offsetList):
-        offset, idx = offsetList[olidx]
-        length = lengths[idx]
-        if offset and tifftools.tifftools.check_offset(srclen, offset, length):
-            src.seek(offset)
-            destOffsets[idx] = dest.tell()
+    destOffsets = [0] * len(original_offsets)
+    original_offsetList = sorted([(offset, idx) for idx, offset in enumerate(original_offsets)])
+    redacted_offsetList = sorted([(offset, idx) for idx, offset in enumerate(redacted_offsets)])
 
-            while (olidx + 1 < len(offsetList) and
-                   offsetList[olidx + 1][0] == offsetList[olidx][0] + lengths[idx] and
-                   tifftools.tifftools.check_offset(srclen, offsetList[olidx + 1][0], lengths[offsetList[olidx + 1][1]])):
-                destOffsets[offsetList[olidx + 1][1]] = destOffsets[idx] + lengths[idx]
-                olidx += 1
-                offset, idx = offsetList[olidx]
-                length += lengths[idx]
-            while length:
-                data = src.read(min(length, COPY_CHUNKSIZE))
-                dest.write(data)
-                length -= len(data)
-        olidx += 1
+    for olidx in tqdm(range(len(original_offsetList))):
+        if is_redacted[olidx]:
+            offset, idx = redacted_offsetList[olidx]
+            length = redacted_lengths[idx]
+            if not tifftools.tifftools.check_offset(redacted_srclen, offset, length):
+                continue
+            src = redacted
+        else:
+            offset, idx = original_offsetList[olidx]
+            length = original_lengths[idx]
+            if not tifftools.tifftools.check_offset(original_srclen, offset, length):
+                continue
+            src = original
+        
+        src.seek(offset)
+        destOffsets[idx] = dest.tell()
+        
+        while length > 0:
+            data = src.read(min(length, COPY_CHUNKSIZE))
+            dest.write(data)
+            length -= len(data)
+    
     return destOffsets
 
 
@@ -75,12 +90,15 @@ def remove_polygons(input_filename: str, output_filename: str, polygons: List[Po
     sourceImage = pyvips.Image.new_from_file(input_filename)
     height, width = sourceImage.height, sourceImage.width
 
-    input_info = tifftools.read_tiff(input_filename)
-    tile_height = get_tiff_tag(input_info, tifftools.Tag.TileHeight.value)[0]
-    tile_width = get_tiff_tag(input_info, tifftools.Tag.TileWidth.value)[0]
-    compression = get_tiff_tag(input_info, tifftools.Tag.Compression.value)[0]
-    jpeg_tables = get_tiff_tag(input_info, tifftools.Tag.JPEGTables.value)
-    jpeg_quality = tifftools.constants.EstimateJpegQuality(jpeg_tables)
+    original_info = tifftools.read_tiff(input_filename)
+    original_tile_height = get_tiff_tag(original_info, tifftools.Tag.TileHeight.value)[0]
+    original_tile_width = get_tiff_tag(original_info, tifftools.Tag.TileWidth.value)[0]
+    original_compression = get_tiff_tag(original_info, tifftools.Tag.Compression.value)[0]
+    original_jpeg_tables = get_tiff_tag(original_info, tifftools.Tag.JPEGTables.value)
+    original_tile_offsets = get_tiff_tag(original_info, tifftools.Tag.TileOffsets.value)['data']
+    original_tile_byte_counts = get_tiff_tag(original_info, tifftools.Tag.TileByteCounts.value)['data']
+
+    original_jpeg_quality = tifftools.constants.EstimateJpegQuality(original_jpeg_tables)
 
     svgStr = f'<svg viewBox="0 0 {width} {height}" xmlns="http://www.w3.org/2000/svg">'
     for polygon in polygons:
@@ -99,35 +117,49 @@ def remove_polygons(input_filename: str, output_filename: str, polygons: List[Po
         outputImage.write_to_file(
             tempfile,
             tile=True,
-            tile_width=tile_width,
-            tile_height=tile_height,
+            tile_width=original_tile_width,
+            tile_height=original_tile_height,
             pyramid=True,
             bigtiff=True,
             compression='jpeg',
-            Q=jpeg_quality
+            Q=original_jpeg_quality
         )
     
-    output_info = tifftools.read_tiff(tempfile)
-    output_tile_height = get_tiff_tag(output_info, tifftools.Tag.TileHeight.value)[0]
-    output_tile_width = get_tiff_tag(output_info, tifftools.Tag.TileWidth.value)[0]
-    output_compression = get_tiff_tag(output_info, tifftools.Tag.Compression.value)[0]
-    output_jpeg_tables = get_tiff_tag(output_info, tifftools.Tag.JPEGTables.value)
+    redacted_info = tifftools.read_tiff(tempfile)
+    redacted_tile_height = get_tiff_tag(redacted_info, tifftools.Tag.TileHeight.value)[0]
+    redacted_tile_width = get_tiff_tag(redacted_info, tifftools.Tag.TileWidth.value)[0]
+    redacted_compression = get_tiff_tag(redacted_info, tifftools.Tag.Compression.value)[0]
+    redacted_jpeg_tables = get_tiff_tag(redacted_info, tifftools.Tag.JPEGTables.value)
+    redacted_tile_offsets = get_tiff_tag(redacted_info, tifftools.Tag.TileOffsets.value)['data']
+    redacted_tile_byte_counts = get_tiff_tag(redacted_info, tifftools.Tag.TileByteCounts.value)['data']
     
-    if (tile_height, tile_width) != (output_tile_height, output_tile_width):
-        raise ValueError('Input tile size does not match output tile size')
-    if compression != output_compression:
-        raise ValueError('Input compression does not match output compression')
-    if jpeg_tables != output_jpeg_tables:
-        raise ValueError('Input JPEG Tables do not match output JPEG Tables')
-    
-    output_tile_offsets = get_tiff_tag(output_info, tifftools.Tag.TileOffset.value)
-    output_tile_byte_counts = get_tiff_tag(output_info, tifftools.Tag.TileByteCounts.value)
+    if (original_tile_height, original_tile_width) != (redacted_tile_height, redacted_tile_width):
+        raise ValueError('Original tile size does not match redacted tile size')
+    if original_compression != redacted_compression:
+        raise ValueError('Original compression does not match redacted compression')
+    if original_jpeg_tables != redacted_jpeg_tables:
+        raise ValueError('Original JPEG Tables do not match redacted JPEG Tables')
 
-    print(f'copying input to file: {output_filename}')
-    shutil.copyfile(input_filename, output_filename)
+    is_redacted = []
+    for offset, length in zip(original_tile_offsets, original_tile_byte_counts):
+        # TODO: calculate x, y coordinates from tile offset
+        x, y = None, None
+        tile_max = overlayImage.extract_area(x, y, original_tile_width, original_tile_height).extract_band(3).max()
+        is_redacted.append(tile_max > 0)
     
-    with open(tempfile, 'rb') as input_file, open(output_filename, 'wb') as output_file:
-        pass
+    with open(output_filename, 'wb') as dest, open(input_filename, 'rb') as original, open(tempfile, 'rb') as redacted:
+        write_redacted_svs(
+            dest=dest,
+            original=original,
+            original_offsets=original_tile_offsets,
+            original_lengths=original_tile_byte_counts,
+            original_srclen=original_info['size'],
+            redacted=redacted,
+            redacted_offsets=redacted_tile_offsets,
+            redacted_lengths=redacted_tile_byte_counts,
+            redacted_srclen=redacted_info['size'],
+            is_redacted=is_redacted,
+        )
 
 
 if __name__ == '__main__':
