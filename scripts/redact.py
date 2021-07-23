58c75c7114863c99cac19827a863f6c33c308c33
Farhan Ahmed <farhan.ahmed@kitware.com>
2021-07-23 16:22:53 -0400

    fixed tile offsets and bytcounts for output tiff

--- scripts/redact.py
+++ scripts/redact.py
@@ -1,10 +1,14 @@
+import argparse
 import json
 import time
 import os
 import shutil
 import struct
-from typing import List, BinaryIO
+from typing import Any, Dict, List, BinaryIO
 from contextlib import contextmanager
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 import pyvips
 import tifftools
@@ -44,18 +48,37 @@ def vips_progress(vips_image: pyvips.Image):
         vips_image.set_progress(False)
 
 
-def get_tiff_tag(info, tag):
+def get_tiff_tag(info: dict, tag: int):
     return info['ifds'][0]['tags'][tag]['data']
 
 
+def get_polygons(annotation_filename: str):
+    with open(annotation_filename, 'r') as f:
+        data = json.load(f)
+    elements = data['annotation']['elements']
+
+    polygons: List[Polygon] = []
+    for e in elements:
+        if e['type'] == 'polyline':
+            points = e['points']
+            fill_color = e['fillColor']
+            line_color = e['lineColor']
+            line_width = e['lineWidth']
+            polygon = Polygon(points, fill_color, line_color, line_width)
+            polygons.append(polygon)
+
+    return polygons
+
 
 def remove_polygons(input_filename: str, output_filename: str, polygons: List[Polygon]):
-    sourceImage = pyvips.Image.new_from_file(input_filename)
+    # sourceImage = pyvips.Image.new_from_file(input_filename)
+    sourceImage = pyvips.Image.tiffload(input_filename)
     height, width = sourceImage.height, sourceImage.width
 
     original_info = tifftools.read_tiff(input_filename)
     original_tile_height = get_tiff_tag(original_info, tifftools.Tag.TileHeight.value)[0]
     original_tile_width = get_tiff_tag(original_info, tifftools.Tag.TileWidth.value)[0]
+    original_photometric = get_tiff_tag(original_info, tifftools.Tag.Photometric.value)[0]
     original_compression = get_tiff_tag(original_info, tifftools.Tag.Compression.value)[0]
     original_jpeg_tables = get_tiff_tag(original_info, tifftools.Tag.JPEGTables.value)
     original_tile_offsets = get_tiff_tag(original_info, tifftools.Tag.TileOffsets.value)
@@ -68,10 +91,6 @@ def remove_polygons(input_filename: str, output_filename: str, polygons: List[Po
         points = ' '.join([f'{pt[0]},{pt[1]}' for pt in polygon.points])
         svgStr += f'<polygon points="{points}" stroke="none" fill="{polygon.fill_color}" />'
     svgStr += '</svg>'
-
-    # TODO: remove saving svg
-    with open(output_filename.replace('out.svs', 'mask.svg'), 'w') as f:
-        f.write(svgStr)
  
     overlayImage = pyvips.Image.svgload_buffer(svgStr.encode())
     outputImage = sourceImage.composite([overlayImage], pyvips.BlendMode.OVER)
@@ -81,13 +100,25 @@ def remove_polygons(input_filename: str, output_filename: str, polygons: List[Po
 
     print(f'outputing to file: {tempfile}')
     with vips_progress(outputImage):
-        outputImage.write_to_file(
+        # outputImage.write_to_file(
+        #     tempfile,
+        #     tile=True,
+        #     tile_width=original_tile_width,
+        #     tile_height=original_tile_height,
+        #     pyramid=True,
+        #     bigtiff=True,
+        #     rgbjpeg=True,
+        #     compression='jpeg',
+        #     Q=original_jpeg_quality
+        # )
+        outputImage.tiffsave(
             tempfile,
             tile=True,
             tile_width=original_tile_width,
             tile_height=original_tile_height,
-            pyramid=True,
+            # pyramid=True,
             bigtiff=True,
+            rgbjpeg=(original_photometric == 2),
             compression='jpeg',
             Q=original_jpeg_quality
         )
@@ -104,84 +135,42 @@ def remove_polygons(input_filename: str, output_filename: str, polygons: List[Po
         raise ValueError('Original tile size does not match redacted tile size')
     if original_compression != redacted_compression:
         raise ValueError('Original compression does not match redacted compression')
-    if original_jpeg_tables != redacted_jpeg_tables:
-        raise ValueError('Original JPEG Tables do not match redacted JPEG Tables')
+    # if original_jpeg_tables != redacted_jpeg_tables:
+    #     raise ValueError('Original JPEG Tables do not match redacted JPEG Tables')
 
     tiff_utils.write_tiff_conditional(original_info['ifds'], redacted_info['ifds'], output_filename, overlayImage, True)
-    return
-
-    print('finding which tiles to redact')
-    is_redacted = []
-    tiles_across = (width + original_tile_width - 1) // original_tile_width
-    tiles_down = (height + original_tile_height - 1) // original_tile_height
-    for i in trange(len(original_tile_offsets)):
-        x = (i % tiles_across) * original_tile_width
-        y = (i // tiles_across) * original_tile_height
-        tile_width = min(original_tile_width, width - x)
-        tile_height = min(original_tile_height, height - y)
-        tile_max = overlayImage.extract_area(x, y, tile_width, tile_height).extract_band(3).max()
-        is_redacted.append(tile_max > 0)
-    
-    print('writing to output svs')
-    with open(output_filename, 'wb') as dest, open(input_filename, 'rb') as original, open(tempfile, 'rb') as redacted:
-        write_tag_data(
-            dest=dest,
-            original=original,
-            original_offsets=original_tile_offsets,
-            original_lengths=original_tile_byte_counts,
-            original_srclen=original_info['size'],
-            redacted=redacted,
-            redacted_offsets=redacted_tile_offsets,
-            redacted_lengths=redacted_tile_byte_counts,
-            redacted_srclen=redacted_info['size'],
-            is_redacted=is_redacted,
-        )
 
 
-def redacted_list(
-    svg: pyvips.Image,
-    width: int,
-    height: int,
-    tile_width: int,
-    tile_height: int,
-    num_tiles: int
-) -> List[bool]:
-    is_redacted: List[bool] = []
-    tiles_across = (width + tile_width - 1) // tile_width
-    tiles_down = (height + tile_height - 1) // tile_height
-
-    for i in trange(len(num_tiles)):
-        x = (i % tiles_across) * tile_width
-        y = (i // tiles_across) * tile_height
-        w = min(tile_width, width - x)
-        h = min(tile_height, height - y)
-        tile_max = svg.extract_area(x, y, w, h).extract_band(3).max()
-        is_redacted.append(tile_max > 0)
-
-    return is_redacted
+def get_args():
+    parser = argparse.ArgumentParser(description='Redact tiff file')
+    parser.add_argument('--input', '--src', type=str, required=True, help='Input image filename')
+    parser.add_argument('--output', '--dest', type=str, required=True, help='Output image filename')
+    parser.add_argument('--annotation', '-a', type=str, required=True, help='Annotation filename')
+    return parser.parse_args()
 
 
-if __name__ == '__main__':
-    input_filename = './downloads/small.svs'
-    output_filename = './downloads/small_out.svs'
-    annotation_filename = './downloads/small.json'
+def write_all(input_filename):
+    info = tifftools.read_tiff(input_filename)
+    for i, ifd in enumerate(info['ifds']):
+        tifftools.write_tiff(ifd, f'./downloads/temp/small_ifd{i}.tiff')
 
-    with open(annotation_filename, 'r') as f:
-        data = json.load(f)
-    elements = data['annotation']['elements']
-    
-    polygons = []
-    for e in elements:
-        if e['type'] == 'polyline':
-            points = e['points']
-            fill_color = e['fillColor']
-            line_color = e['lineColor']
-            line_width = e['lineWidth']
-            polygon = Polygon(points, fill_color, line_color, line_width)
-            polygons.append(polygon)
-    
+
+def main(args):
+    input_filename = args.input
+    output_filename = args.output
+    annotation_filename = args.annotation
+
     start = time.time()
+
+    polygons = get_polygons(annotation_filename)
     remove_polygons(input_filename, output_filename, polygons)
+    # write_all(input_filename)
+
     end = time.time()
     elapsed = time.strftime('%M:%S', time.gmtime(end - start))
     print(f'total time: {elapsed}')
+
+
+if __name__ == '__main__':
+    args = get_args()
+    main(args)
--- scripts/tiff_utils.py
+++ scripts/tiff_utils.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import struct
 
@@ -31,40 +32,60 @@ def write_tiff_conditional(
         ifdPtr = len(header) - 8
         dest.write(header)
 
-        ifdPtr = write_ifd_conditional(dest, bom, original_ifds[0], redacted_ifds[0], ifdPtr, svg)
+        is_redacted = redacted_list(svg, original_ifds[0])
+
+        ifd = conditional_ifd(original_ifds[0], redacted_ifds[0], is_redacted)
+        ifdPtr = write_ifd_conditional(dest, bom, ifd, original_ifds[0], redacted_ifds[0], ifdPtr, is_redacted)
+        # ifdPtr = write_ifd(dest, bom, True, redacted_ifds[0], ifdPtr)
+        # ifdPtr = write_ifd(dest, bom, True, ifd, ifdPtr)
 
         for ifd in original_ifds[1:]:
             ifdPtr = write_ifd(dest, bom, True, ifd, ifdPtr)
 
 
-def find_directory(
+def conditional_ifd(
     original_ifd: Dict[str, dict],
-    redacted_ifds: List[Dict[str, dict]],
+    redacted_ifd: Dict[str, dict],
+    is_redacted: List[bool],
 ):
-    try:
-        original_tile_height = original_ifd['tags'][Tag.TileHeight.value]['data'][0]
-        original_tile_width = original_ifd['tags'][Tag.TileWidth.value]['data'][0]
-    except:
-        return None
+    ifd = copy.deepcopy(original_ifd)
+    tile_offsets = []
+    tile_bytecounts = []
 
-    for ifd in redacted_ifds:
-        tile_height = ifd['tags'][Tag.TileHeight.value]['data'][0]
-        tile_width = ifd['tags'][Tag.TileWidth.value]['data'][0]
-        if (tile_height, tile_width) == (original_tile_height, original_tile_width):
-            return ifd
-    return None
+    original_tile_offsets = original_ifd['tags'][Tag.TileOffsets.value]['data']
+    original_tile_bytecounts = original_ifd['tags'][Tag.TileByteCounts.value]['data']
+    redacted_tile_offsets = redacted_ifd['tags'][Tag.TileOffsets.value]['data']
+    redacted_tile_bytecounts = redacted_ifd['tags'][Tag.TileByteCounts.value]['data']
+
+    offset = original_tile_offsets[0]
+    for i, redacted in enumerate(is_redacted):
+        if redacted:
+            bytecount = redacted_tile_bytecounts[i]
+        else:
+            bytecount = original_tile_bytecounts[i]
+        tile_offsets.append(offset)
+        tile_bytecounts.append(bytecount)
+        offset += bytecount
+
+    ifd['tags'][Tag.TileOffsets.value]['data'] = tile_offsets
+    ifd['tags'][Tag.TileByteCounts.value]['data'] = tile_bytecounts
+
+    return ifd
+
 
 def redacted_list(
     svg: pyvips.Image,
-    width: int,
-    height: int,
-    tile_width: int,
-    tile_height: int,
-    num_tiles: int
+    ifd: Dict[str, dict],
 ) -> List[bool]:
+    width = ifd['tags'][Tag.ImageWidth.value]['data'][0]
+    height = ifd['tags'][Tag.ImageHeight.value]['data'][0]
+    tile_width = ifd['tags'][Tag.TileWidth.value]['data'][0]
+    tile_height = ifd['tags'][Tag.TileHeight.value]['data'][0]
+
     is_redacted: List[bool] = []
     tiles_across = (width + tile_width - 1) // tile_width
     tiles_down = (height + tile_height - 1) // tile_height
+    num_tiles = tiles_across * tiles_down
 
     print('calculating redacted tiles')
     for i in trange(num_tiles):
@@ -82,9 +103,10 @@ def write_ifd_conditional(
     dest: BinaryIO,
     bom: str,
     ifd: Dict[str, dict],
+    original_ifd: Dict[str, dict],
     redacted_ifd: Dict[str, dict],
     ifdPtr: int,
-    svg: pyvips.Image
+    is_redacted: List[bool],
 ):
     ptrpack = 'Q'
     tagdatalen = 8
@@ -93,7 +115,7 @@ def write_ifd_conditional(
     subifdPtrs: Dict[TiffConstant, int] = {}
     tagSet = Tag
 
-    with OpenPathOrFobj(ifd['path_or_fobj'], 'rb') as src, OpenPathOrFobj(redacted_ifd['path_or_fobj'], 'rb') as redacted_src:
+    with OpenPathOrFobj(ifd['path_or_fobj'], 'rb') as src:
         for tag, taginfo in sorted(ifd['tags'].items()):
             tag = get_or_create_tag(
                 tag,
@@ -110,31 +132,26 @@ def write_ifd_conditional(
 
             if tag.isOffsetData():
                 if tag.value == Tag.TileOffsets.value:
-                    width = ifd['tags'][Tag.ImageWidth.value]['data'][0]
-                    height = ifd['tags'][Tag.ImageHeight.value]['data'][0]
-                    tile_width = ifd['tags'][Tag.TileWidth.value]['data'][0]
-                    tile_height = ifd['tags'][Tag.TileHeight.value]['data'][0]
-                    num_tiles = len(data)
-                    is_redacted = redacted_list(svg, width, height, tile_width, tile_height, num_tiles)
-                    data = write_tag_data_conditional(
-                        dest,
-                        src,
-                        data,
-                        ifd['tags'][int(tagSet[tag.bytecounts])]['data'],
-                        ifd['size'],
-                        redacted_src,
-                        redacted_ifd['tags'][Tag.TileOffsets.value]['data'],
-                        redacted_ifd['tags'][Tag.TileByteCounts.value]['data'],
-                        redacted_ifd['size'],
-                        is_redacted
-                    )
+                    with OpenPathOrFobj(original_ifd['path_or_fobj'], 'rb') as original_src, OpenPathOrFobj(redacted_ifd['path_or_fobj'], 'rb') as redacted_src:
+                        data = write_tag_data_conditional(
+                            dest=dest,
+                            original_src=original_src,
+                            original_offsets=redacted_ifd['tags'][Tag.TileOffsets.value]['data'],
+                            original_lengths=redacted_ifd['tags'][Tag.TileByteCounts.value]['data'],
+                            original_srclen=original_ifd['size'],
+                            redacted_src=redacted_src,
+                            redacted_offsets=redacted_ifd['tags'][Tag.TileOffsets.value]['data'],
+                            redacted_lengths=redacted_ifd['tags'][Tag.TileByteCounts.value]['data'],
+                            redacted_srclen=redacted_ifd['size'],
+                            is_redacted=is_redacted
+                        )
                 elif isinstance(tag.bytecounts, str):
                     data = write_tag_data(
-                        dest,
-                        src,
-                        data,
-                        ifd['tags'][int(tagSet[tag.bytecounts])]['data'],
-                        ifd['size']
+                        dest=dest,
+                        src=src,
+                        offsets=data,
+                        lengths=ifd['tags'][int(tagSet[tag.bytecounts])]['data'],
+                        srclen=ifd['size']
                     )
                 else:
                     data = write_tag_data(dest, src, data, [tag.bytecounts] * count, ifd['size'])
@@ -220,35 +237,35 @@ def write_tag_data_conditional(
 ):
     COPY_CHUNKSIZE = 1024 ** 2
 
-    if len(original_offsets) != len(original_lengths) or len(redacted_offsets) != len(redacted_lengths):
-        raise Exception('Offsets and byte counts do not correspond')
+    if len(original_offsets) != len(original_lengths):
+        raise TifftoolsException('Original image ffsets and byte counts do not correspond')
+    if len(redacted_offsets) != len(redacted_lengths):
+        raise TifftoolsException('Redacted image Offsets and byte counts do not correspond')
     
     destOffsets = [0] * len(original_offsets)
     original_offsetList = sorted([(offset, idx) for idx, offset in enumerate(original_offsets)])
     redacted_offsetList = sorted([(offset, idx) for idx, offset in enumerate(redacted_offsets)])
 
     print('writing redacted files')
-    for olidx in trange(len(original_offsetList)):
+    for olidx in trange(len(is_redacted)):
         if is_redacted[olidx]:
             offset, idx = redacted_offsetList[olidx]
             length = redacted_lengths[idx]
-            if not check_offset(redacted_srclen, offset, length):
-                continue
-            # src = redacted_src
-            src = original_src
+            src = redacted_src
+            srclen = redacted_srclen
         else:
             offset, idx = original_offsetList[olidx]
             length = original_lengths[idx]
-            if not check_offset(original_srclen, offset, length):
-                continue
             src = original_src
-        
-        src.seek(offset)
-        destOffsets[idx] = dest.tell()
-        
-        while length > 0:
-            data = src.read(min(length, COPY_CHUNKSIZE))
-            dest.write(data)
-            length -= len(data)
-    
+            srclen = original_srclen
+
+        if offset and check_offset(srclen, offset, length):
+            src.seek(offset)
+            destOffsets[idx] = dest.tell()
+
+            while length:
+                data = src.read(min(length, COPY_CHUNKSIZE))
+                dest.write(data)
+                length -= len(data)
+
     return destOffsets
